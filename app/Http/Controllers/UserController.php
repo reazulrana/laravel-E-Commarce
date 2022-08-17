13786cdf74965948c3b6856cff0e444660ab737a
Reazul Islam (Rana) <rana12312344@gmail.com>
2022-08-17 16:38:26 +0600

    Login Done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class UserController extends Controller
+{
+    //
+    function login(Request $req){
+
+
+
+       $user=  User::where(['email'=>$req->email])->first();
+
+    //    return $user->password;
+       if(!$user || !Hash::check($req->password,$user->password)){
+        return "Password Is Not Matched";
+       }
+       else{
+        $req->session()->put('user',$user);
+        
+        return redirect('/');
+
+       }
+    }
+}
